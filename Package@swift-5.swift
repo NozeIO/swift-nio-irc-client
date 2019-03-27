7aae5c28e50db8919d3bb1b995db0cadfc940e7c
Helge Heß <devteam@zeezide.de>
2019-03-27 15:28:10 +0100

    Use swift-nio-irc 0.7.0 on Swift 5
    NIO 2 Swift 5 enabled.

--- Package@swift-5.swift
+++ Package@swift-5.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", 
                  from: "2.0.0"),
         .package(url: "https://github.com/SwiftNIOExtras/swift-nio-irc.git",
-                 from: "0.6.1")
+                 from: "0.7.0")
     ],
     targets: [
         .target(name: "IRC", dependencies: [ "NIOIRC" ])
