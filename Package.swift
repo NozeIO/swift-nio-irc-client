b3612d6409a980f5f4f4cac2ca9d83e26e263934
Helge Heß <devteam@zeezide.de>
2021-01-13 18:19:09 +0100

    Up the NIO deps
    ... only the latest is the greatest.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 var packageDependencies : [ Package.Dependency ] = [
     .package(url:  "https://github.com/apple/swift-nio.git",
-             from: "2.18.0"),
+             from: "2.25.1"),
     .package(url:  "https://github.com/SwiftNIOExtras/swift-nio-irc.git",
              from: "0.8.0")
 ]
@@ -12,7 +12,7 @@ var targetDependencies : [ Target.Dependency ] = [ "NIO", "NIOIRC" ]
 if #available(OSX 10.14, iOS 12.0, tvOS 12.0, watchOS 6.0, *) {
     packageDependencies += [
         .package(url:  "https://github.com/apple/swift-nio-transport-services",
-                 from: "1.6.0")
+                 from: "1.9.1")
     ]
     targetDependencies += [ "NIOTransportServices" ]
 }
